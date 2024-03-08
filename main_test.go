72ae6837b5fde65e0e6ae4fac6974f749c8f344c
ejones77 <e77jones@gmail.com>
2024-03-07 23:28:20 -0600

    first pass

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package trimmed_mean
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrimmedMean(t *testing.T) {
+	arr := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	percent := 0.2
+	expected := 5.5
+	result, err := TrimmedMean(arr, percent)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, result)
+}
